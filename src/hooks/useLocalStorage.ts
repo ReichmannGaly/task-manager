d9aa2d44d1427c59feaf5ae3a6708e926f1d0813
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-06-15 12:12:49 +0300

    feat: implement useLocalStorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,18 @@
-const useLocalStorage = () => {
+import { useLayoutEffect, useState } from "react";
 
+const useLocalStorage = <T>(name: string) => {
+  const [state, setState] = useState<T>();
+
+  useLayoutEffect(() => {
+    if (window) setState(JSON.parse(localStorage.getItem(name) || "{}"))
+  }, [name])
+
+  return {
+    save: (values: T) => {
+      localStorage.setItem(name, JSON.stringify(values || {}))
+      setState(values)
+    }, read: () => state
+  }
 }
 
 export {
