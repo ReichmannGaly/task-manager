3db420486d847cc70120dd78f5fe2497384d9214
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-06-15 12:01:22 +0300

    Revert "feat: create useTaskManager custom hook"
    This reverts commit aa92fc2e42720ee9890721d6f3ddb4484b476ac1.

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,5 +1,4 @@
 import React, { ChangeEvent, useRef } from 'react';
-import {useTaskManager} from "@/store/useTaskManager";
 
 interface Task {
   id: number,
@@ -8,69 +7,68 @@ interface Task {
 }
 
 const TaskManager = () => {
-  const createTaskRef = useRef<HTMLInputElement>(null);
-  const {
-    tasks,
-    searchTask,
-    addTask,
-    updateTask,
-    deleteTask,
-    setSearchTask,
-  } = useTaskManager();
+  // const createTaskRef = ...:
+  // const {
+  //   tasks,
+  //   searchTask,
+  //   addTask,
+  //   updateTask,
+  //   deleteTask,
+  //   setSearchTask,
+  // } = useTaskManager();
 
   const handleAddTask = () => {
-    const title = createTaskRef.current!.value; // Replace with the value in the createTaskRef
+    const title = ""; // Replace with the value in the createTaskRef 
     const newTask = {
       id: Date.now(),
       title,
       completed: false,
     };
-    addTask(newTask);
+    // addTask(newTask);
   };
 
   const handleUpdateTask = (taskId: number, updatedTask: Task) => {
-    updateTask(taskId, updatedTask);
+    // updateTask(taskId, updatedTask);
   };
 
   const handleDeleteTask = (taskId: number) => {
-    deleteTask(taskId);
+    // deleteTask(taskId);
   };
 
   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchTask(e.target.value);
+    // setSearchTask(e.target.value);
   };
 
-  const filteredTasks = tasks.filter((task) =>
-    task.title.toLowerCase().includes(searchTask.toLowerCase())
-  );
+  // See! I already give you everything!
+  // const filteredTasks = tasks.filter((task) =>
+  //   task.title.toLowerCase().includes(searchTask.toLowerCase())
+  // );
 
   return (
     <div>
       <h1>Task Manager</h1>
 
-      <input type="text" ref={createTaskRef}/>
+      <input type="text" /*ref={}*//>
 
       <button onClick={handleAddTask}>Add Task</button>
 
       <input type="text" onChange={handleSearch} placeholder="Search Task" />
 
       <ul>
+        {/* 
         {filteredTasks.map((task) => (
           <li key={task.id}>
             <input
               type="text"
               value={task.title}
               onChange={(e) =>
-                handleUpdateTask(task.id, {
-                    title: e.target.value,
-                    id: task.id,
-                    completed: task.completed
-                })
+                handleUpdateTask(task.id, { title: e.target.value })
               }
             />
             <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
           </li>
         ))}
+        */}
       </ul>
     </div>
   );
--- src/store/useTaskManager.ts
+++ src/store/useTaskManager.ts
@@ -1,48 +1,6 @@
-import create from 'zustand';
+const useTaskManager = () => {
 
-interface Task {
-  id: number;
-  title: string;
-  completed: boolean;
 }
-
-interface TaskManagerState {
-  tasks: Task[];
-  addTask: (task: Task) => void;
-  updateTask: (taskId: number, updatedTask: Partial<Task>) => void;
-  deleteTask: (taskId: number) => void;
-  searchTask: string;
-  setSearchTask: (searchTerm: string) => void;
-}
-
-const useTaskManager = create<TaskManagerState>((set) => ({
-  tasks: [],
-  addTask: (task) => {
-    set((state) => ({
-      tasks: [...state.tasks, task], // Add the new task to the tasks array
-    }));
-  },
-  updateTask: (taskId, updatedTask) => {
-    set((state) => ({
-      tasks: state.tasks.map((task) =>
-          task.id === taskId ? { ...task, ...updatedTask } : task // Update the specified task with the new details
-      ),
-    }));
-  },
-  deleteTask: (taskId) => {
-    set((state) => ({
-      tasks: state.tasks.filter((task) => task.id !== taskId), // Remove the task with the specified ID from the tasks array
-    }));
-  },
-  searchTask: '',
-  setSearchTask: (searchTerm) => {
-    set((state) => ({
-      ...state,
-      searchTask: searchTerm,
-    }));
-  },
-}));
-
 
 export {
   useTaskManager
